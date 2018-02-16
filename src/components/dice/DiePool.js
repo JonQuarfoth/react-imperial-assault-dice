3c98b1a033f1436208617352302a120ad338ca07
Jon Quarfoth <jon.quarfoth@gmail.com>
2018-02-16 14:32:29 -0600

    Add Targets

--- src/components/dice/DiePool.js
+++ src/components/dice/DiePool.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
-import { Button } from 'react-bootstrap';
+import { Button, ButtonToolbar } from 'react-bootstrap';
 import isEmpty from 'lodash/isEmpty'
 import RollStats from './RollStats'
 
 const blue = {
 	id: 'blue',
+	className: 'primary',
 	outcomes: [
 		{ range: 2, damage: 0, surge: 1 },
 		{ range: 2, damage: 1, surge: 0 },
@@ -16,6 +17,7 @@ const blue = {
 }
 const green = {
 	id: 'green',
+	className: 'success',
 	outcomes: [
 		{ range: 1, damage: 0, surge: 1 },
 		{ range: 1, damage: 1, surge: 1 },
@@ -27,6 +29,7 @@ const green = {
 }
 const yellow = {
 	id: 'yellow',
+	className: 'warning',
 	outcomes: [
 		{ range: 0, damage: 0, surge: 1 },
 		{ range: 0, damage: 1, surge: 2 },
@@ -38,6 +41,7 @@ const yellow = {
 }
 const red = {
 	id: 'red',
+	className: 'danger',
 	outcomes: [
 		{ range: 0, damage: 1, surge: 0 },
 		{ range: 0, damage: 2, surge: 0 },
@@ -64,15 +68,20 @@ class DiePool extends Component {
 	}
 
 	render() {
-		const dice = this.state.dice.map((die, index) => <li key={index}>{die.id.toUpperCase()}</li>)
+
+		const dice = this.state.dice.map((die, index) => 
+			<li key={index} className={'text-' + die.className}>{die.id.toUpperCase()}</li>
+		)
 		return (
 			<div>
 				<div>
-					<Button onClick={this.addBlue}>+ Blue</Button>
-					<Button onClick={this.addGreen}>+ Green</Button>
-					<Button onClick={this.addYellow}>+ Yellow</Button>
-					<Button onClick={this.addRed}>+ Red</Button>
-					<Button disabled={isEmpty(this.state.dice)} onClick={this.clearPool}>Reset</Button>
+					<ButtonToolbar>
+						<Button bsSize="large" bsStyle={blue.className} onClick={this.addBlue}>+ Blue</Button>
+						<Button bsSize="large" bsStyle={green.className} onClick={this.addGreen}>+ Green</Button>
+						<Button bsSize="large" bsStyle={yellow.className} onClick={this.addYellow}>+ Yellow</Button>
+						<Button bsSize="large" bsStyle={red.className} onClick={this.addRed}>+ Red</Button>
+						<Button bsSize="large" disabled={isEmpty(this.state.dice)} onClick={this.clearPool}>Reset</Button>
+					</ButtonToolbar>
 				</div>
 				<div>
 					Current Dice:
--- src/components/dice/RollStats.js
+++ src/components/dice/RollStats.js
@@ -4,16 +4,39 @@ import head from 'lodash/head'
 import tail from 'lodash/tail'
 import isEmpty from 'lodash/isEmpty'
 import size from 'lodash/size'
+import filter from 'lodash/filter'
 import { Grid, Row, Col } from 'react-bootstrap';
-
+import DiceFilter from './DiceFilter'
+import { FormGroup, ControlLabel, FormControl } from 'react-bootstrap'
+import ProbabilityPercent from './ProbabilityPercent'
 
 class RollStats extends Component {
 
 	constructor() {
 		super();
+		this.state = {
+			minRange: 0,
+			minDamage: 0,
+			minSurge: 0
+		}
 		this.generateOutcomes = this.generateOutcomes.bind(this);
 		this.combineOutcomes = this.combineOutcomes.bind(this);
 		this.generateStats = this.generateStats.bind(this);
+		this.changeMinRange = this.changeMinRange.bind(this);
+		this.changeMinDamage = this.changeMinDamage.bind(this);
+		this.changeMinSurge = this.changeMinSurge.bind(this);
+	}
+
+	changeMinRange(minRange) {
+		this.setState({minRange: minRange});
+	}
+
+	changeMinDamage(minDamage) {
+		this.setState({minDamage: minDamage});
+	}
+
+	changeMinSurge(minSurge) {
+		this.setState({minSurge: minSurge});
 	}
 
 	render() {
@@ -22,12 +45,18 @@ class RollStats extends Component {
 			return null;
 		}
 
-		let stats = this.generateStats(outcomes);
+		let filteredOutcomes = filter(outcomes, (outcome) => 
+			outcome.range >= this.state.minRange && outcome.damage >= this.state.minDamage && outcome.surge >= this.state.minSurge
+		)
+		const probability = size(filteredOutcomes) / size(outcomes)
+
+		let stats = this.generateStats(filteredOutcomes);
 		
 		return (
 			<Grid>
 				<Row>
 					<Col xs={3}>
+						<DiceFilter label="Target Range" id="minRange" value={this.state.minRange} onChange={this.changeMinRange} />
 						<h3>Range</h3>
 						<ul>
 							<li>Min: {stats.range.min}</li>
@@ -36,6 +65,7 @@ class RollStats extends Component {
 						</ul>
 					</Col>
 					<Col xs={3}>
+						<DiceFilter label="Target Damage" id="minDamage" value={this.state.minDamage} onChange={this.changeMinDamage} />
 						<h3>Damage</h3>
 						<ul>
 							<li>Min: {stats.damage.min}</li>
@@ -44,12 +74,21 @@ class RollStats extends Component {
 						</ul>
 					</Col>
 					<Col xs={3}>
+						<DiceFilter label="Target Surges" id="minSurge" value={this.state.minSurge} onChange={this.changeMinSurge} />
 						<h3>Surge</h3>
 						<ul>
 							<li>Min: {stats.surge.min}</li>
 							<li>Max: {stats.surge.max}</li>
 							<li>Avg: {stats.surge.avg}</li>
 						</ul>
+					</Col>
+					<Col xs={3}>
+					    <FormGroup>
+					      <ControlLabel>Chance To Roll at least target</ControlLabel>
+					      <FormControl.Static>
+					      	<ProbabilityPercent value={probability}/>
+					      </FormControl.Static>
+					    </FormGroup>
 					</Col>
 				</Row>
 			</Grid>
